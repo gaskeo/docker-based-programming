d95e12e0550efbeedc38f49ab1e29d3e055be90c
gaskeo <zeytunyan2004@mail.ru>
2023-03-29 17:46:01 +0500

    feat(compiler): add compiler

---
+++ compile.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+mkdir -p '.compiled'
+cp "Dockerfile" ".compiled/Dockerfile.compiled"
+
+echo "- copy" $'\e[1;32m'Dockerfile$'\e[0m' "to" $'\e[1;32m'.compiled./Dockerfile.compiled$'\e[0m'
+
+has_include_lines=1
+check_count=0
+
+while [ "$has_include_lines" = 1 ]
+do
+    (( check_count++ ))
+    echo -e "\n- start checking Dockerfile.compiled file" $'\e[1;32mN'"$check_count"$'\e[0m'
+    file_tmp=$(cat ".compiled/Dockerfile.compiled")
+
+    include_lines_raw=$(echo "$file_tmp" | grep "# INCLUDE *")
+    readarray -t include_lines <<<"$include_lines_raw"
+
+    if [ "$include_lines_raw" = "" ];
+        then
+            echo -e "\n\n-=- compile finished in $check_count iterations -=-"
+            has_include_lines=0
+            break
+    fi
+
+    echo "--- found" $'\e[1;32m'"${#include_lines[@]}"$'\e[0m' "INCLUDE directives"
+
+    for include_line in "${include_lines[@]}"
+    do
+        file_name=$(echo "$include_line" | sed 's/# INCLUDE //g' | sed 's/\r//g')
+        file_content=$(cat "$file_name")
+        echo "--- touch $file_name"
+
+        file_tmp=$(echo "$file_tmp" | awk -v include_line="$include_line" \
+            -v file_content="$file_content" \
+            '{ if($0 == include_line) print file_content; else print $0}')
+    done
+
+    echo "$file_tmp" > .compiled/Dockerfile.compiled
+    echo '- save tmp in .compiled/Dockerfile.compiled'
+done
