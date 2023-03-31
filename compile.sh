36e833bf2917a9bbc5fa644de3d76cd13047c26a
gaskeo <zeytunyan2004@mail.ru>
2023-03-31 12:31:29 +0500

    fix(compile): fix bugs with quotes and backslashes

--- compile.sh
+++ compile.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+docker_files_raw=$(find . -name "Dockerfile*")
+docker_files=($docker_files_raw)
+
 mkdir -p '.compiled'
 cp "Dockerfile" ".compiled/Dockerfile.compiled"
 
@@ -10,11 +13,11 @@ check_count=0
 
 while [ "$has_include_lines" = 1 ]
 do
-    (( check_count++ ))
+    let "check_count++"
     echo -e "\n- start checking Dockerfile.compiled file" $'\e[1;32mN'"$check_count"$'\e[0m'
     file_tmp=$(cat ".compiled/Dockerfile.compiled")
 
-    include_lines_raw=$(echo "$file_tmp" | grep "# INCLUDE *")
+    include_lines_raw=$(echo "$file_tmp" | grep "^# INCLUDE*")
     readarray -t include_lines <<<"$include_lines_raw"
 
     if [ "$include_lines_raw" = "" ];
@@ -24,17 +27,18 @@ do
             break
     fi
 
-    echo "--- found" $'\e[1;32m'"${#include_lines[@]}"$'\e[0m' "INCLUDE directives"
+    echo "--- found" $'\e[1;32m'"${#include_lines[@]}"$'\e[0m' "INCLUDE direcives"
 
     for include_line in "${include_lines[@]}"
     do
         file_name=$(echo "$include_line" | sed 's/# INCLUDE //g' | sed 's/\r//g')
-        file_content=$(cat "$file_name")
+        file_content="$(<$file_name)"
+        file_content="${file_content//\\/\\\\}"
         echo "--- touch $file_name"
 
         file_tmp=$(echo "$file_tmp" | awk -v include_line="$include_line" \
             -v file_content="$file_content" \
-            '{ if($0 == include_line) print file_content; else print $0}')
+            '{ if($0 == include_line)  print file_content; else print $0}')
     done
 
     echo "$file_tmp" > .compiled/Dockerfile.compiled
